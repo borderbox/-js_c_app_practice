2a4b8d02fbee9017a2132cd69ebfa305ba7e03e5
borderbox <example@gmail.com>
2021-06-22 13:58:29 +0300

    minor mistakes fixe

--- script.js
+++ script.js
@@ -11,9 +11,11 @@ let personalMovieDB = {
 };
 
 const ask1 = prompt(`One of last film?`, `logan`);
-const answer1 = prompt(`Mark of ${ask1}`, `7`);
+const answer1 = prompt(`Mark of "${ask1}"`, `7`);
 const ask2 = prompt(`One of last film?`, `red`);
-const answer2 = prompt(`Mark of ${ask1}`, `6`);
+const answer2 = prompt(`Mark of "${ask2}"`, `6`);
 
 personalMovieDB.movies[ask1] = answer1;
 personalMovieDB.movies[ask2] = answer2;
+
+console.log(personalMovieDB);
